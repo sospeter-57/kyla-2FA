572ca46790422a7e71ebdb76d0a8e69d9c8ba5c5
sospeter-57 <sospeterkinyanjui57@gmail.com>
2026-03-11 10:10:03 +0300

    gui with fyne qt binddings here we come

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
-	"kyla-2FA/utils"
 	"log"
 	"time"
 
+	"kyla-2FA/utils"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"github.com/pquerna/otp/totp"
 )
 
@@ -13,10 +17,21 @@ func main() {
 	randomBytes := utils.GenerateRandomBytes(32)
 	encodedSecret := utils.EncodeBase64(randomBytes)
 
+	// create the main window
+	app := app.New()
+	window := app.NewWindow("kyla-2FA")
+	window.CenterOnScreen()
+	window.Resize(fyne.NewSize(700, 500))
+
+	// create a label widget
+	label := widget.NewLabel("just kidding here and there with fyne qt bindings")
+	window.SetContent(label)
+
 	code, err := totp.GenerateCode(encodedSecret, time.Now())
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
 
 	fmt.Println("TOTP code: ", code)
+	window.ShowAndRun()
 }
